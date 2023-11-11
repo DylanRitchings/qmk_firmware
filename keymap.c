bf0e1f2d81c31387785cb897343c3b0915de9845
DylanRitchings <43646210+DylanRitchings@users.noreply.github.com>
2023-11-11 16:46:00 +0000

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -4,9 +4,6 @@
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define CTL_ALT_SHFT \
-    (LCTL(LALT(KC_LSFT)))
-
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -15,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTL,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-  CTL_ALT_SHFT(), KC_LALT, KC_PSCR, KC_LGUI, _LOWER,   KC_SPC,  KC_ENT,  _RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  LCTL(LALT(KC_LSFT)), KC_LALT, KC_PSCR, KC_LGUI, _LOWER,   KC_SPC,  KC_ENT,  _RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_LOWER] = LAYOUT_preonic_grid(

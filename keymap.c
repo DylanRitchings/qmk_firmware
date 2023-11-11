2a28be61b0b77de1f163b0fac9efd6599a1e7741
DylanRitchings <43646210+DylanRitchings@users.noreply.github.com>
2023-11-11 17:19:39 +0000

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTL,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-  LCTL(LALT(KC_LSFT)), KC_LALT, KC_PSCR, KC_LGUI, _LOWER,   KC_SPC,  KC_ENT,  _RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  LCTL(LALT(KC_LSFT)), KC_LALT, KC_PSCR, KC_LGUI, LOWER,   KC_SPC,  KC_ENT,  RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_LOWER] = LAYOUT_preonic_grid(
@@ -32,3 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 
 };
+
+void matrix_init_user(void) {
+    // Set the initial backlight color to white
+    // This function may vary depending on your keyboard
+    // Use the appropriate QMK backlight function for your keyboard
+    set_backlight_color(RGB_WHITE);
+}
